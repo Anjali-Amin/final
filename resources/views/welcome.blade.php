cad78af595b19743690c3bff76759bcf1fee8914
Anjali Amin <aminanjali12@gmail.com>
2020-07-02 00:20:24 +0530

    updated using bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,1 @@
-<html>
-    <head>
-        <style>
-            
-        </style>
-        <meta charset="utf-8">
-        <link href="sticky-footer-navbar.css" rel="stylesheet">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    </head>
-    <body>
-       @include('include.navbar')
-</body>
-  <html>
+@extends('layouts.app')
